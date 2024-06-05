baa96649d2b9e00d8bc848d7ca6c29d59f6d2faa
raihank43 <raihankusuma5@gmail.com>
2024-06-05 20:21:21 +0700

    feat: moving directory for simplicity

---
+++ src/app/[boards]/page.tsx
@@ -0,0 +1,3 @@
+export default function Boards({ params }: { params: { boards: string } }) {
+  return <h1>Hello {params.boards}</h1>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col px-20  min-h-screen">
+      <div className="flex justify-center">
+        <h1 className="flex items-center text-6xl font-bold p-10 text-red-900">
+          <img src="/indochan-logo.png" className="h-32"></img>
+          Indochan
+        </h1>
+      </div>
+
+      <div className="border-solid border-red-700 border-2 mb-2 bg-white">
+        <h1 className="bg-red-900 font-bold text-white">What is IndoChan?</h1>
+        <p className="text-sm">
+          IndoChan is an Indonesian modern imageboard built using Nextjs. It is a work
+          in progress and is not yet ready for production.
+        </p>
+        <Button>Click me</Button>
+      </div>
+      <div className="border-solid border-red-700 border-2 bg-white mb-2">
+        <h1 className="bg-orange-300 font-bold text-red-900">Boards</h1>
+        <div className="bg-slate-200 flex">
+          <div className=""></div>
+          <Link href={"/random"} className="font-bold">Random</Link>
+        </div>
+      </div>
+
+      <div className="border-solid border-red-700 border-2 bg-white">
+        <h1 className="bg-orange-300 font-bold text-red-900">
+          Popular Threads
+        </h1>
+        <div className="bg-white flex">
+          <div className=""></div>
+          <p>Random</p>
+        </div>
+      </div>
+    </main>
+  );
+}
