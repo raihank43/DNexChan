b7718907bf723c6d46d279c8369e393a510e5891
raihank43 <raihankusuma5@gmail.com>
2024-06-27 17:02:16 +0700

    fix: hydration error

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,13 +1,10 @@
 export default function FooterComponent() {
   return (
-    <footer className="flex flex-col gap-2 justify-center mx-10 py-3 border-y-2  border-orange-300 my-10 bg-orange-200 rounded-lg shadow-lg ">
-      <p className="text-center font-semibold text-red-900">
-        &copy; {new Date().getFullYear()}{" "} IndoChan
-        <p className="text-red-900 hover:text-orange-300 ease-out duration-300 text-sm">
-          All trademarks and copyrights on this page are owned by their
-          respective parties. Images uploaded are the responsibility of the
-          Poster. Comments are owned by the Poster.
-        </p>
+    <footer className="flex flex-col justify-center mx-10 py-3 border-y-2 px-2  border-orange-300 my-10 bg-orange-100 rounded-lg shadow-lg ">
+      <p className="text-red-900 hover:text-orange-300 ease-out duration-300 text-sm font-semibold text-center">
+        All trademarks and copyrights on this page are owned by their respective
+        parties. Images uploaded are the responsibility of the Poster. Comments
+        are owned by the Poster.
       </p>
     </footer>
   );
